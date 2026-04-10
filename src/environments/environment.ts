9d7634dfbe494f30a579448bac5d9de610638152
Martin Falcon <martin.falcon@hotmail.com>
2026-04-10 13:26:35 -0600

    correccion variables de entorno

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,6 @@
  */
 export const environment = {
   production: false,
-  supabaseUrl: 'https://bdenieusmjzrpvfeoixa.supabase.co',
-  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJkZW5pZXVzbWp6cnB2ZmVvaXhhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3MjQ4MjgsImV4cCI6MjA5MTMwMDgyOH0.1jcf1d5BvSY025hP7sN0qTSEjeDYmVRvp0Qm4a2cCu4'
+  supabaseUrl: '__SUPABASE_URL__',
+  supabaseAnonKey: '__SUPABASE_ANON_KEY__'
 };
